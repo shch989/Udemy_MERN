04f09c38cea65c2c702547a08a06591da56d8f34
shch989 <tankpop11@gmail.com>
2023-01-15 17:42:03 +0900

    UI Elements 적용

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
 function Users() {
-  return <h2>Users Works!</h2>
+  const USRS = [
+    {
+      id: 'u1',
+      name: 'Seong Hyeon',
+      image:
+        'https://image.jtbcplus.kr/data/contents/jam_photo/202301/11/d4b19cb8-f1cd-4a0d-8c06-a12eef725d21.jpg',
+      places: 3,
+    },
+  ]
+
+  return <UsersList items={USRS} />
 }
 
 export default Users
