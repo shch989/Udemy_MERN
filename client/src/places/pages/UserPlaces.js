f973825b1b411d5e622ffbccf091e2c28902f8cc
shch989 <tankpop11@gmail.com>
2023-01-16 18:22:38 +0900

    Param 추가

--- client/src/places/pages/UserPlaces.js
+++ client/src/places/pages/UserPlaces.js
@@ -1,38 +1,41 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import PlaceList from '../components/PlaceList'
 
-function UserPlaces() {
-  const DUMMY_PLACES = [
-    {
-      id: 'p1',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl:
-        'https://media.cnn.com/api/v1/images/stellar/prod/130802164459-skyscrapers-gallery-empire-state-building.jpg?q=w_3000,h_1955,x_0,y_0,c_fill',
-      adress: '20 W 34th St, New York, NY 1001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584,
-      },
-      creator: 'u1'
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://media.cnn.com/api/v1/images/stellar/prod/130802164459-skyscrapers-gallery-empire-state-building.jpg?q=w_3000,h_1955,x_0,y_0,c_fill',
+    adress: '20 W 34th St, New York, NY 1001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
     },
-    {
-      id: 'p2',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl:
-        'https://media.cnn.com/api/v1/images/stellar/prod/130802164459-skyscrapers-gallery-empire-state-building.jpg?q=w_3000,h_1955,x_0,y_0,c_fill',
-      adress: '20 W 34th St, New York, NY 1001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584,
-      },
-      creator: 'u2'
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://media.cnn.com/api/v1/images/stellar/prod/130802164459-skyscrapers-gallery-empire-state-building.jpg?q=w_3000,h_1955,x_0,y_0,c_fill',
+    adress: '20 W 34th St, New York, NY 1001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
     },
-  ]
+    creator: 'u2',
+  },
+]
 
-  return <PlaceList items={DUMMY_PLACES} />
+function UserPlaces() {
+  const userId = useParams().userId
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId)
+  return <PlaceList items={loadedPlaces} />
 }
 
 export default UserPlaces
