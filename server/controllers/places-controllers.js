19e10202a4bd9095896b6835c58f9391d890d615
shch989 <tankpop11@gmail.com>
2023-01-21 17:58:10 +0900

    DELETE 기능 추가

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -2,7 +2,7 @@ const uuid = require('uuid')
 
 const HttpError = require('../models/http-error')
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: 'p1',
     title: 'Empire State Building',
@@ -50,9 +50,29 @@ exports.createPlace = (req, res, next) => {
     description,
     location: coordinates,
     address,
-    creator
+    creator,
   }
 
   DUMMY_PLACES.push(createPlace)
-  res.status(201).json({place: createPlace})
+  res.status(201).json({ place: createPlace })
+}
+
+exports.updatePlace = (req, res, next) => {
+  const { title, description } = req.body
+  const placeId = req.params.pid
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) }
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId)
+  updatedPlace.title = title
+  updatedPlace.description = description
+
+  DUMMY_PLACES[placeIndex] = updatedPlace
+
+  res.status(200).json({ place: updatedPlace })
+}
+
+exports.deletePlace = (req, res, next) => {
+  const placeId = req.params.pid
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId)
+  res.status(200).json({ message: 'Deleted place.' })
 }
