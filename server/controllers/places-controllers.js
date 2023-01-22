b135c7d72c31c1b0297352e8af44cc517e06ed7a
shch989 <tankpop11@gmail.com>
2023-01-23 00:23:22 +0900

    User api 입력 검증

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -45,7 +45,7 @@ exports.getPlacesByUserId = (req, res, next) => {
 
 exports.createPlace = (req, res, next) => {
   const errors = validationResult(req)
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     console.log(errors)
     throw new HttpError('Invalid inputs passed, please check your data.', 422)
   }
@@ -65,6 +65,12 @@ exports.createPlace = (req, res, next) => {
 }
 
 exports.updatePlace = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log(errors)
+    throw new HttpError('Invalid inputs passed, please check your data.', 422)
+  }
+
   const { title, description } = req.body
   const placeId = req.params.pid
 
@@ -80,6 +86,9 @@ exports.updatePlace = (req, res, next) => {
 
 exports.deletePlace = (req, res, next) => {
   const placeId = req.params.pid
+  if(!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError('Could not find a place for that id.', 404)
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId)
   res.status(200).json({ message: 'Deleted place.' })
 }
