e5e284cdf993dc2f90764eb78239a406ce7b24ba
shch989 <tankpop11@gmail.com>
2023-01-23 23:13:14 +0900

    Place models

---
+++ server/models/place.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+let placeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  location: {
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+  },
+  creator: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('Place', placeSchema)
