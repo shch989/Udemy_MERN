62c34bc7a1af02ace9ab616676a09b06bf48d424
shch989 <tankpop11@gmail.com>
2023-01-23 23:05:09 +0900

    몽구스 연결

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
-
 const HttpError = require('./models/http-error')
 
 const app = express()
@@ -20,13 +20,20 @@ app.use((req, res, next) => {
 })
 
 app.use((error, req, res, next) => {
-  if(res.headerSent) {
+  if (res.headerSent) {
     return next(error)
   }
   res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'})
+  res.json({ message: error.message || 'An unknown error occurred!' })
 })
 
-app.listen(5000, () => {
-  console.log(`Example app listening at http://localhost:5000/`)
-})
+mongoose
+  .connect('mongodb://localhost:27017/places')
+  .then(() => {
+    app.listen(5000, () => {
+      console.log(`Example app listening at http://localhost:5000/`)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
