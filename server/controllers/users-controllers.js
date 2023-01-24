78103213656167224b007c37e94a2c5b5f0d48cc
shch989 <tankpop11@gmail.com>
2023-01-24 12:23:23 +0900

    모든 유저 정보 불러오기

--- server/controllers/users-controllers.js
+++ server/controllers/users-controllers.js
@@ -13,8 +13,18 @@ const DUMMY_USERS = [
   },
 ]
 
-exports.getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS })
+exports.getUsers = async (req, res, next) => {
+  let users
+  try {
+    users = await User.find({}, '-password')
+  } catch (err) {
+    const error = new HttpError(
+      'Fetching users failed, please try again later.',
+      500
+    )
+    return next(error)
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) })
 }
 
 exports.signup = async (req, res, next) => {
