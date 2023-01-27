b28adb1498edf294be7d72c6221cd33147875053
shch989 <tankpop11@gmail.com>
2023-01-27 23:18:42 +0900

    마무리

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App'
+
+import { createRoot } from 'react-dom/client';
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
